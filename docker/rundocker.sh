e41d243af1bc00f600f6ffbd64163a03cc82dffb
ToRapture <torapture@gmail.com>
2020-06-10 18:12:09 +0800

    rm container before run judger

--- docker/rundocker.sh
+++ docker/rundocker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+IMAGE_NAME="nenuoj-judger"
+CONTAINER_NAME="nenuoj-judger"
+
+docker container rm -f $CONTAINER_NAME 1>/dev/null 2>&1
+
 docker run \
   -d \
   --restart always \
-  --name=nenuoj-judger \
+  --name=$CONTAINER_NAME \
   -v $CONFIG_FILE_PATH:/judger/config.ini \
   -v $TEST_FILES_PATH:/judger/test_files \
   -v $SPJ_FILES_PATH:/judger/spj_files \
-  nenuoj-judger
+  $IMAGE_NAME
