99d27154925d770b237016f82c3d8265a48ef75f
ToRapture <torapture@gmail.com>
2020-06-19 12:22:38 +0800

    fix ptrace in docker

--- docker/rundocker.sh
+++ docker/rundocker.sh
@@ -8,6 +8,7 @@ docker container rm -f $CONTAINER_NAME 1>/dev/null 2>&1
 docker run \
   -d \
   --restart always \
+  --cap-add=SYS_PTRACE \
   --name=$CONTAINER_NAME \
   -v $CONFIG_FILE_PATH:/judger/config.ini \
   -v $TEST_FILES_PATH:/judger/test_files \
