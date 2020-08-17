04e8a631c05560113805e158f046388481b21c47
ToRapture <torapture@gmail.com>
2020-08-17 16:22:57 +0800

    update docker

--- docker/rundocker.sh
+++ docker/rundocker.sh
@@ -13,4 +13,5 @@ docker run \
   -v $CONFIG_FILE_PATH:/judger/config.ini \
   -v $TEST_FILES_PATH:/judger/test_files \
   -v $SPJ_FILES_PATH:/judger/spj_files \
+  -p $HOSTPORT:$CONTAINERPORT \
   $IMAGE_NAME
